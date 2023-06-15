9f379e5e91b01f8589ae6fb43101303713a02b70
vamshiurumadla09 <urumadlavamshi@gmail.com>
2023-06-15 11:35:13 +0530

    first commit

--- src/store/reducers/combineReducer.js
+++ src/store/reducers/combineReducer.js
@@ -4,6 +4,5 @@ import resume from './resumeReducer';
 import skills from './skillReducer';
 import projects from './projectsReducer';
 
-
 const reducer = combineReducers({header:header, skills:skills, projects:projects,});
 export default reducer;
