00978376a03db2ce9475e1635744fd23e6db02ca
vamshiurumadla09 <urumadlavamshi@gmail.com>
2023-06-26 15:29:50 +0530

    added datafect

--- src/store/reducers/projectsReducer.js
+++ src/store/reducers/projectsReducer.js
@@ -1,6 +1,7 @@
 import ecommerce from '../../portfolioImages/ecommerce.jpeg';
 import todoAdvance from '../../portfolioImages/todoAdvance.png';
 import todoBasic from '../../portfolioImages/todoBasic.png';
+import dataFetch from '../../portfolioImages/dataFetch.png'
 
 const initialState = [
         {
@@ -14,6 +15,12 @@ const initialState = [
            description:`Advance todo list with good features to use it. A todo list app is a digital tool designed to help individuals organize, manage, and track their tasks and responsibilities.`,
             image:todoAdvance,
             link:'https://6495286f5110e000078812a8--singular-arithmetic-c1d366.netlify.app/'
+        },
+        {
+            title:'data fetch app',
+           description:`app to fetch the data for api call. this app fetch the data from the api and display it on screen. its easy to fetch the data of any api by using this app`,
+            image:dataFetch,
+            link:'https://64995f4adf9c3f1e42a9445e--loquacious-valkyrie-dc1072.netlify.app/'
         },
         {
             title:'todo Basic',
